e5700bb616c2b665010e3a5ae8ffc4b542c88c9b
Phuc Pham <phameeek@gmail.com>
2022-11-14 20:32:49 +0100

    Implement a repeatString function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    let repeatedString = '';
+    if (number >= 0) {
+        for (let i = 0; i < number; i++) {
+            repeatedString += string;
+        }
+        return repeatedString;
+    } else {
+        return 'ERROR';
+    }
 };
 
 // Do not edit below this line
