5b586ee6d2570da39c4447b41674205091a10d31
Phuc Pham <phameeek@gmail.com>
2022-11-14 20:56:56 +0100

    Implement a sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum, secondNum) {
+    let result = 0;
+    if (firstNum < 0 || secondNum < 0 || typeof firstNum !== "number" || typeof secondNum !== "number") {
+        return 'ERROR';
+    }
+    if (secondNum > firstNum) {
+        for (let i = firstNum; i <= secondNum; i++) {
+            result += i;
+        }
+    }
+    for (let i = secondNum; i <= firstNum; i++) {
+        result += i;
+    }
+    return result;
 };
 
 // Do not edit below this line
