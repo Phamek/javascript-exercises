3166db7ea41be954c34acf4ffe189444b155ef0c
Phuc Pham <phameeek@gmail.com>
2022-11-14 21:08:33 +0100

    Implement a leapYears function

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,6 @@
-const leapYears = function() {
-
+const leapYears = function(leapYear) {
+    if (leapYear % 4 !== 0 || (leapYear % 100 === 0 && leapYear % 400 !== 0)) return false;
+    return true;
 };
 
 // Do not edit below this line
