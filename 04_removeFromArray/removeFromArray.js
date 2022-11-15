715415feddd6b6d742c4c0bd698fdd63bcc49613
Phuc Pham <phameeek@gmail.com>
2022-11-15 22:10:48 +0100

    Implement a removeFromArray function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    let newArray = [...array];
+    for (let i = 0; i < array.length; i++) {
+        if (newArray.includes(args[i])) {
+            newArray.splice(newArray.indexOf(args[i]), 1);
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
